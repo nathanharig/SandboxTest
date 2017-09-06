06f0020e985276fdcd3ab3fd14673ce5bfb60fa2
nathanharig <nathanharig@gmail.com>
2017-09-06 14:03:44 -0400

    Updated to single app

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,4 @@
-import React, { Component } from 'react';
-import { View, Sectionlist, Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import App from './src/app';
 
-class Test extends Component {
-
-  render() {
-    return (
-      <View>
-        <Text> Hello World </Text>
-      </View>
-    );
-  };
-}
-
-AppRegistry.registerComponent('Test', () => Test);
+AppRegistry.registerComponent('Test', () => App);
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header, Button, CardItem, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+
+}
+
+export default App;
